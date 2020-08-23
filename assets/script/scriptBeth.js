2136c35d723e71334a245e3906bc39dfe8d256e2
Luna <dlramos194@gmail.com>
2020-08-23 17:35:03 -0500

    added weather to JS

--- assets/script/scriptBeth.js
+++ assets/script/scriptBeth.js
@@ -1,3 +1,50 @@
+function renderWeatherbyLocation() {
+    $("#mainBtn").on("click", function() {
+        event.preventDefault();
+        console.log("click!")
+
+        // function to render current weather:
+        var apiKey = "631af5c78fdde025e0d500219377445c";
+        var userLocation = $("#enterCity").val().trim();
+        var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + userLocation + "&appid=" + apiKey;
+
+        console.log(userLocation)
+
+        $.ajax({
+                url: queryURLWeather,
+                method: "GET"
+            })
+            .then(function(response) {
+                console.log(response);
+
+                // Temp from K to F
+                var Farenheit = (response.main.temp - 273.15) * 1.80 + 32;
+                console.log(Farenheit.toFixed(2))
+
+                //getting the icon
+                iconID = response.weather[0].icon
+
+                console.log(iconID)
+
+                //  setting the text
+
+                $(".current-weather").text("  " + " " + userLocation + " " + " " + Farenheit.toFixed(1) + " °F");
+
+                //  adding attributes: 
+                $(".icon").attr("src", " https://openweathermap.org/img/wn/" + iconID + ".png")
+
+            })
+    })
+}
+
+
+
+
+renderWeatherbyLocation();
+
+
+
+
 function wineSelection() {
 
     var temp = 60
@@ -5,8 +52,8 @@ function wineSelection() {
     var wineName = ""
 
     var catalog = [
-        ['Stephen King', 'Rose', 'Sauvignon Blanc', 'Pinot Noir'], 
-        ['Fyodor Dostoevsky', 'Malbec', 'Merlot', 'Pinot Noir'], 
+        ['Stephen King', 'Rose', 'Sauvignon Blanc', 'Pinot Noir'],
+        ['Fyodor Dostoevsky', 'Malbec', 'Merlot', 'Pinot Noir'],
         ['James Patterson', 'Cabernet Sauvignon', 'Shiraz', 'Riesling'],
         ['Agatha Christie', 'Zinf Andel', 'Viognier', 'Merlot/Pinot Gris'],
         ['Isabel Allende', 'Torrentes', 'Syrah', 'Garnacha'],
@@ -16,15 +63,16 @@ function wineSelection() {
         ['Olga Tokarczuk', 'Chenin Blanc', 'Reisling', 'Prosecco'],
         ['Cormac McCarthy', 'Malbec', 'Chianti', 'Sauvignon Blanc'],
         ['Aldous Huxley', 'Mulled Wine', 'Pinot Noir', 'Pinot Grigio'],
-        ['Penn Jillette', 'Decaf Caffè Americano w/ 8 shots of Decaf Espresso', 'Decaffinated Coffee', 'Sparkling Water']]
+        ['Penn Jillette', 'Decaf Caffè Americano w/ 8 shots of Decaf Espresso', 'Decaffinated Coffee', 'Sparkling Water']
+    ]
 
-            
+
 
     for (i = 0; i < exampleList.length; i++) {
         if ((authorClicked === catalog[i][0]) && (temp <= 60)) {
             wineName = catalog[i][1]
         }
-        if ((authorClicked === catalog[i][0]) && (temp > 60 && temp < 80)){
+        if ((authorClicked === catalog[i][0]) && (temp > 60 && temp < 80)) {
             wineName = catalog[i][2]
         }
         if ((authorClicked === catalog[i][0]) && (temp >= 80)) {
@@ -33,37 +81,37 @@ function wineSelection() {
     }
 
     return wineName
-    
+
 }
 
 
 var listName = "combined print fiction" // best seller books in this section 
 var queryURLNYT = "https://api.nytimes.com/svc/books/v3/lists/current/" + listName + ".json?api-key=E7hqgdVcY5GIbLAfpBFL6tvAVz8oV8WG";
-        console.log(queryURLNYT)
-      $.ajax({
+console.log(queryURLNYT)
+$.ajax({
         url: queryURLNYT,
         method: "GET"
-      })
-        .then(function(response) {
-            console.log(response);
-            
-            // console.log(response.items.0.saleInfo.buyLink);
-            var randInt = Math.floor(Math.random() * 20)
-            
-            // console.log(response.results.books[randInt].author)
+    })
+    .then(function(response) {
+        console.log(response);
 
-            var surprise = []
-            var surpriseWine = ['Malbec', 'Cabernet Sauvignon', 'Rose', 'Pinot Noir', 'Pinot Grigio', 'Shiraz', 'Chardonnay','Zinfandel', 'Sauvigon Blanc']
-            var wineIndex = Math.floor(Math.random()* 9)
-            console.log(surpriseWine[wineIndex]) // randomly chooses wine type for the surpise book 
+        // console.log(response.items.0.saleInfo.buyLink);
+        var randInt = Math.floor(Math.random() * 20)
 
+        // console.log(response.results.books[randInt].author)
 
-            surprise.push(response.results.books[randInt].author)
-            surprise.push(response.results.books[randInt].title)
-            surprise.push(response.results.books[randInt].book_image)
-            surprise.push(response.results.books[randInt].description)
-            surprise.push(response.results.books[randInt].amazon_product_url)
-            console.log(surprise)
+        var surprise = []
+        var surpriseWine = ['Malbec', 'Cabernet Sauvignon', 'Rose', 'Pinot Noir', 'Pinot Grigio', 'Shiraz', 'Chardonnay', 'Zinfandel', 'Sauvigon Blanc']
+        var wineIndex = Math.floor(Math.random() * 9)
+        console.log(surpriseWine[wineIndex]) // randomly chooses wine type for the surpise book 
 
 
-        });
+        surprise.push(response.results.books[randInt].author)
+        surprise.push(response.results.books[randInt].title)
+        surprise.push(response.results.books[randInt].book_image)
+        surprise.push(response.results.books[randInt].description)
+        surprise.push(response.results.books[randInt].amazon_product_url)
+        console.log(surprise)
+
+
+    });
